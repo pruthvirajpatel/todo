10e956a099369513b50a96b660efd65fbd8e3a5c
Pruthviraj patel <pruthviraj0505@gmail.com>
2025-12-03 06:30:50 +0530

    Basic setup with Vite,Typescript and Jest
    Basic setup with Vite,Typescript and Jest

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json'}]
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/src/__mocks__/fileMock.ts'
+  }
+};
---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders Vite React app", () => {
+  render(<App />);
+  expect(screen.getByText("Vite + React")).toBeInTheDocument();
+});
