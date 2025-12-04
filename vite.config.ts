30864e45b7cccf51c1651b3b6c090044673b8dd0
Pruthviraj patel <pruthviraj0505@gmail.com>
2025-12-05 00:55:47 +0530

    build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {},
+  plugins: [
+    react(),
+
+    visualizer({
+      filename: "bundle-stats.html",
+      template: "treemap",
+      gzipSize: true,
+      brotliSize: true,
+      open: true,
+    }),
+  ],
+
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+    esbuildOptions: {
+      target: "esnext",
     },
   },
-})
+
+  build: {
+    target: "esnext",
+    modulePreload: true,
+    cssCodeSplit: true,
+    sourcemap: false,
+    minify: "esbuild",
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+
+    chunkSizeWarningLimit: 800,
+  },
+});
